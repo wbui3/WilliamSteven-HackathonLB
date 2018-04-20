2cff588dd7806ecf4519d2502ae2ac1283458375
wbui3 <buiquangwilliam@yahoo.com>
2018-04-20 15:02:36 -0700

    added a sample program

---
+++ sample.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	std::cout << "Goodbye World!\n";
+
+	vector<int> Vector1;	// This is william's vector 
+	std::cout << "William's vector contains: ";
+	for (int i = 0; i < 10; i++) { Vector1.push_back(i); }
+	for (int x : Vector1) {
+		cout << x; 
+	}
+
+
+	return 0;
+}
